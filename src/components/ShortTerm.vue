bbb7e359e6c0a295d9f1d054c2fd2af4e151cb0f
shektor <s.hektor@gmail.com>
2019-07-08 11:04:31 +0100

    Modify prop type passed to SummaryItems
    Passing an object will allow for an easier way to access relevant
    elements that need to be passed to other components in SummaryItems.

--- src/components/ShortTerm.vue
+++ src/components/ShortTerm.vue
@@ -3,11 +3,14 @@
     Your short term goal
     <h1>{{ career.name }}</h1>
     <SummaryItems
-      :items="[
-        'Salary +' + salaryDifference + '%',
-        career.details.demandLevel + ' market demand',
-        career.details.experienceYears + ' years experience'
-      ]"
+      :items="{
+        salary: {
+          current: career.details.meanSalary,
+          goal: currentSalary
+        },
+        demandLevel: career.details.demandLevel,
+        experienceYears: career.details.experienceYears
+      }"
     />
   </div>
 </template>
